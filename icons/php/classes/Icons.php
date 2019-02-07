5b48d092f2a29ffaf85404beb178726f14d4b8e0
Lauren Hamel <lauren.hamel@outlook.com>
2019-02-07 13:34:30 -0500

    Fixes icon PHP to check for exact IDs first then near IDs

--- icons/php/classes/Icons.php
+++ icons/php/classes/Icons.php
@@ -25,13 +25,26 @@ class Icons {
   // Getter
   function __get( $id ) {
     
-    // Retrieve icons by ID.
+    // Try to retrieve icons by exact ID.
     $icon = array_values(array_filter($this->icons, function($svg) use ($id) {
       
       // Find the icon file with the given ID.
-      return preg_match("/^{$id}/", $svg);
+      return "{$id}.svg" == $svg;
       
     }));
+    
+    // Otherwise, try to retrieve icons by near ID.
+    if( empty($icon) ) {
+      
+      // Retrieve icons by near ID.
+      $icon = array_values(array_filter($this->icons, function($svg) use ($id) {
+
+        // Otherwise, find the icon file with the closest ID.
+        return preg_match("/^{$id}/", $svg);
+
+      }));
+      
+    }
                          
     // Ignore invalid icons.
     if( empty($icon) ) return;
