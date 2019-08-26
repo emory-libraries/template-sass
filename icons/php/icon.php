7eb34e13915aa64c6104385c37cbca85eeed8e4e
Lauren Hamel <lauren.hamel@emory.edu>
2019-08-26 16:13:59 -0400

    Adds caching to icons displayed as background images

--- icons/php/icon.php
+++ icons/php/icon.php
@@ -20,29 +20,37 @@ $icon = $icons->{$icon};
 
 // Verify that icon exists.
 if( $icon ) {
-  
+
   // Get icon mods.
   unset($params['id']);
-  
+
   // Allow the icon to be modified.
   foreach( $params as $mod => $args ) {
-    
+
     // Verify that the modifier exists.
     if( method_exists($icon, $mod) ) {
-    
+
       // Modify the icon by passing array arguments.
       if( is_array($args) ) call_user_func_array([$icon, $mod], $args);
 
       // Otherwise, modify the icon by passing simple arguments.
       else $icon->{$mod}($args);
-      
+
     }
-    
+
   }
-  
+
+  // Set expiration time in days.
+  $expires = 30;
+
+  // Send cache headers.
+  header('Expires: '.gmdate("D, d M Y H:i:s", time() + (86400 * $expires)) . " GMT");
+  header('Pragma: cache');
+  header('Cache-control: max-age=' . (86400 * $expires));
+
   // Return the icon SVG.
   echo $icon->svg;
-  
+
 }
 
 ?>
